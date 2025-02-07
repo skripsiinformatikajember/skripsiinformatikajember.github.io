fadcc15b460201bb4f6d81cacfd5dd07321e463c
Skripsi Informatika Jember <skripsi.tijbr@gmail.com>
2025-02-07 20:53:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Skripsi Informatika Jember</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="keywords" content="Aplikasi / Sistem | Jurusan TI SI MI Dan Lainnya | Kampus Area Bandung | SPK Mining SIG PCD IoT Game IS | % Finish Sejak 2007.">   
+
+	<meta name="description" content="Aplikasi / Sistem | Jurusan TI SI MI Dan Lainnya | Kampus Area Bandung | SPK Mining SIG PCD IoT Game IS | % Finish Sejak 2007.">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+</head>
+<body>
+    <?php
+        header('location: https://www.instagram.com/skripsiinformatikajember/');
+        exit();
+    ?>
+</body>
+</html>
